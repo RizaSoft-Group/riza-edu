592194a3344f1bbac93362bf8599c6674a1b0e49
bw007 <mr.khasanov02@gmail.com>
2025-12-25 23:31:31 +0500

    feat: add main layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <app-header></app-header>
+  <router-view></router-view>
+</template>
+
+<script setup>
+import AppHeader from '@/components/layout/AppHeader.vue'
+</script>
+
+<style lang="css" scoped>
+
+</style>
