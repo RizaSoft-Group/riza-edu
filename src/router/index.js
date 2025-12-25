53de25286ca5e5f8bf28cbdb466997adb9648dbf
bw007 <mr.khasanov02@gmail.com>
2025-12-25 23:36:34 +0500

    router config

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,14 @@
+import MainLayout from '@/layouts/MainLayout.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
-    // {
-    //   path: '/login',
-    //   name: 'login',
-    //   component: Login
-    // },
-    // {
-    //   path: '/register',
-    //   name: 'register',
-    //   component: Register
-    // },
-    // {
-    //   path: '/profile',
-    //   name: 'profile',
-    //   component: Profile
-    // }
+    {
+      path: '/',
+      name: 'MainLayout',
+      component: MainLayout
+    }
   ],
 })
 
