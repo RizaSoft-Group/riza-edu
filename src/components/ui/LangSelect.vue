ed82c3bf66393bd325defcb639decbbad7e352bc
bw007 <mr.khasanov02@gmail.com>
2025-12-25 23:33:57 +0500

    feat: add i18n switcher

---
+++ src/components/ui/LangSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-5 h-3 cursor-pointer overflow-hidden rounded-xs">
+    <img :src="flag" class="h-full object-cover" alt="uz">
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const flag = ref('/uz-flag.svg')
+const lang = [
+  {
+    value: 'uz',
+    flag: '/uz-flag.svg',
+  },
+  {
+    value: 'en',
+    flag: '/uk-flag.svg',
+  },
+  {
+    value: 'ru',
+    flag: '/ru-flag.svg',
+  },
+  {
+    value: 'ja',
+    flag: '/ja-flag.svg',
+  }
+]
+
+</script>
