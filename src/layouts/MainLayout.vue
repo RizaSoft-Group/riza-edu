0a42bafb69e3a6002c8c26f2260aca53aebc5aa3
Shaxzod <isomiddinovshaxzod007@gmail.com>
2025-12-26 21:08:54 +0500

    homepage added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,14 @@
 <template lang="html">
   <app-header></app-header>
+  <app-homepage />
   <router-view></router-view>
+  <app-footer />
 </template>
 
 <script setup>
+import AppFooter from '@/components/layout/AppFooter.vue';
 import AppHeader from '@/components/layout/AppHeader.vue'
+import AppHomepage from '@/components/layout/AppHomepage.vue';
 </script>
 
 <style lang="css" scoped>
