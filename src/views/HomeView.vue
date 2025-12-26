7349f058d6e1caf381bde5072f17a3dde7f6ec49
bw007 <mr.khasanov02@gmail.com>
2025-12-27 00:12:16 +0500

    feat: add HomeView component

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="font-medium">Welcome to Home Page</h1>
+    <p class="text-sm">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Doloremque eveniet cumque error ut consectetur voluptates,
+      consequuntur ea voluptatibus doloribus. In ipsam mollitia facilis
+      velit cumque blanditiis tenetur omnis molestiae aut.
+    </p>
+  </div>
+</template>
