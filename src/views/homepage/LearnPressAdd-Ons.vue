3dc164cc89aaf615e24c359c747b3e56d36125a1
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-03 20:54:09 +0500

    The LearnePressaAdd-On component has been created.

---
+++ src/views/homepage/LearnPressAdd-Ons.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <div class="container  my-20 mx-auto px-30">
+        <div class="learnpress  max-h-80 p-15">
+            <div class="learnpress-content w-sm">
+                <p class="text-semibold">GET MORE POWER FROM</p>
+                <h1 class="text-4xl font-bold">LearnPress Add-On</h1>
+                <p class="mt-5 mb-5">The next level of LearnPress - LMS WordPress Plugin. More Powerful, Flexible and
+                    Magical Inside</p>
+                <el-button class="!bg-orange-500 !text-white !p-5 !rounded-full">
+                    Explorer Course
+                </el-button>
+            </div>
+        </div>
+    </div>
+
+</template>
+<style scoped>
+.learnpress {
+    background-image: linear-gradient(to right, rgba(127, 255, 195, 0.5), rgba(255, 87, 160, 0.5));
+    border-radius: 15px;
+}
+</style>
