55bd88d68135a5c931983a6ae537b89cf7e7fca5
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-03 20:42:46 +0500

    The homepage folder was opened in views and all components related to the homepage were added.

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
         {
           path: '',
           name: 'Home',
-          component: () => import('@/views/HomeView.vue'),
+          component: () => import('@/views/homepage/HomeView.vue'),
         }
       ],
     }
