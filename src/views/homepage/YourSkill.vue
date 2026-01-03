2ac11c381cc1a8aed786c6bfa18f1b6e8f5dd9c8
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-03 20:49:13 +0500

    The yourskill component on the homepage has been created.

---
+++ src/views/homepage/YourSkill.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container mx-auto p-30">
+        <div class="grid lg:grid-cols-2 grid-cols-1 gap-50">
+            <div class="skill-image">
+                <img src="/yourskill/image.png" alt="skill-image" class="w-full h-full order-1 md:order-none">
+            </div>
+
+            <div class="skill-content">
+                <h1 class="font-bold text-3xl lg:text-4xl leading-tight mb-4">Grow Us Your Skill With LearnPress LMS</h1>
+                <p class="mt-6 mb-6 text-gray-500">We denounce with righteous indignation and dislike men who are so beguiled and
+                    demoralized that cannot trouble.</p>
+                <div class="check flex align-center mb-3">
+                    <el-icon size="20px" color="#00f" class="!text-green-600 !font-bold">
+                        <Check />
+                    </el-icon>
+                    <h5 class="ms-2">Certification</h5>
+                </div>
+                <div class="check flex align-center mb-3">
+                    <el-icon size="20px" color="#00f" class="!text-green-600 !font-bold">
+                        <Check />
+                    </el-icon>
+                    <h5 class="ms-2">Certification</h5>
+                </div>
+                <div class="check flex align-center mb-3">
+                    <el-icon size="20px" color="#00f" class="!text-green-600 !font-bold">
+                        <Check />
+                    </el-icon>
+                    <h5 class="ms-2">Certification</h5>
+                </div>
+                <div class="check flex align-center mb-3">
+                    <el-icon size="20px" color="#00f" class="!text-green-600 !font-bold">
+                        <Check />
+                    </el-icon>
+                    <h5 class="ms-2">Certification</h5>
+                </div>
+
+                <el-button class="!rounded-full !p-5 !bg-orange-500 !text-white !mt-5">Explorer Course</el-button>
+            </div>
+        </div>
+    </div>
+</template>
