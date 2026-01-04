0a3b42d433f6950de79a952a0ecccbb37f6b757a
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-04 14:32:34 +0500

    responsive rolled

--- src/views/homepage/TopCategories.vue
+++ src/views/homepage/TopCategories.vue
@@ -5,13 +5,13 @@
                 <h1 class="font-bold text-3xl mb-3">Top Categories</h1>
                 <p>Explore our Popular Categories</p>
             </div>
-            <<router-link to="courses" class="rounded-[50px] px-2 py-1 border-2 transition duration-300 tranform hover:!bg-orange-500 hover:!text-white hover:!border-orange-500">All Categories</router-link>
+            <router-link to="allcategories" class="rounded-[50px] px-3 py-1.5 border-2 border-gray-300 transition duration-300 tranform hover:bg-orange-500! hover:text-white! hover:border-orange-500!">All Categories</router-link>
         </div>
 
-        <div class="categories-cards grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-8 mt-10">
+        <div class="categories-cards grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 mt-10">
             <router-link to="/art-design"
-                class="block rounded-xl bg-white py-15 px-10 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15 px-auto lg:min-w-6 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <EditPen />
                 </el-icon>
 
@@ -25,8 +25,8 @@
             </router-link>
 
             <router-link to="/art-development"
-                class="block rounded-xl bg-white py-15 px-10 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15 px-auto lg:min-w-auto text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <ElementPlus />
                 </el-icon>
 
@@ -40,8 +40,8 @@
             </router-link>
 
             <router-link to="/art-comunacation"
-                class="block rounded-xl bg-white py-15 px-10 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15 px-auto text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <Share />
                 </el-icon>
 
@@ -55,8 +55,8 @@
             </router-link>
 
             <router-link to="/art-videography"
-                class="block rounded-xl bg-white py-15 px-10 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15 px-auto text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <Film />
                 </el-icon>
 
@@ -70,8 +70,8 @@
             </router-link>
 
             <router-link to="/art-photography"
-                class="block rounded-xl bg-white py-15 px-10 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15 px-auto text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <Camera />
                 </el-icon>
 
@@ -85,8 +85,8 @@
             </router-link>
 
             <router-link to="/art-marketing"
-                class="block rounded-xl bg-white py-15 px-10 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15 px-auto text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <Money />
                 </el-icon>
 
@@ -100,8 +100,8 @@
             </router-link>
 
             <router-link to="/art-content"
-                class="block rounded-xl bg-white py-15 px-10 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15 px-auto text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <Tickets />
                 </el-icon>
 
@@ -115,8 +115,8 @@
             </router-link>
 
             <router-link to="/art-finance"
-                class="block rounded-xl bg-white py-15 px-10 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15 px-auto text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <OfficeBuilding />
                 </el-icon>
 
@@ -130,8 +130,8 @@
             </router-link>
 
             <router-link to="/art-scince"
-                class="block rounded-xl bg-white p-15 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15  text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <Cpu />
                 </el-icon>
 
@@ -145,12 +145,12 @@
             </router-link>
 
             <router-link to="/art-network"
-                class="block rounded-xl bg-white p-15 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
-                <el-icon size="48" class="mb-4 !text-orange-500">
+                class="block rounded-xl bg-white py-15 text-center shadow-sm transition duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:text-orange-500">
+                <el-icon size="48" class="mb-4 text-orange-500!">
                     <Comment />
                 </el-icon>
 
-                <h3 class="mb-1 font-semibold text-lg  transition-colors duration-300 group-hover:!text-orange-500">
+                <h3 class="mb-1 font-semibold text-lg  transition-colors duration-300 group-hover:text-orange-500!">
                     Network
                 </h3>
 
