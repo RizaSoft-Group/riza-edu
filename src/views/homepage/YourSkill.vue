a3ff46e7eb59f78567e7ae3b49a05c04a99ca113
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-05 17:28:00 +0500

    yourskill has been made responsive

--- src/views/homepage/YourSkill.vue
+++ src/views/homepage/YourSkill.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container mx-auto p-30">
-        <div class="grid lg:grid-cols-2 grid-cols-1 gap-50">
+    <div class="container mx-auto px-5 py-22.5">
+        <div class="grid lg:grid-cols-2 grid-cols-1 xl:gap-40 gap-15 items-center ">
             <div class="skill-image">
-                <img src="/yourskill/image.png" alt="skill-image" class="w-full h-full order-1 md:order-none">
+                <img src="/yourskill/image.png" alt="skill-image" class="w-full order-1">
             </div>
 
             <div class="skill-content">
@@ -10,7 +10,7 @@
                 <p class="mt-6 mb-6 text-gray-500">We denounce with righteous indignation and dislike men who are so beguiled and
                     demoralized that cannot trouble.</p>
                 <div class="check flex align-center mb-3">
-                    <el-icon size="20px" color="#00f" class="!text-green-600 !font-bold">
+                    <el-icon size="20px" color="#00f" class="text-green-600! font-bold!">
                         <Check />
                     </el-icon>
                     <h5 class="ms-2">Certification</h5>
@@ -34,7 +34,9 @@
                     <h5 class="ms-2">Certification</h5>
                 </div>
 
-                <el-button class="!rounded-full !p-5 !bg-orange-500 !text-white !mt-5">Explorer Course</el-button>
+                <el-button type="warning" round size="large">
+                    Explorer Course
+                </el-button>
             </div>
         </div>
     </div>
