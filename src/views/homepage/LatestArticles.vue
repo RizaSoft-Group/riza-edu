13906f6ba1c438d0e672c5ea466f96f63fd96906
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-05 21:42:55 +0500

    latest updated

--- src/views/homepage/LatestArticles.vue
+++ src/views/homepage/LatestArticles.vue
@@ -3,7 +3,7 @@
         <div class="latest">
             <div class="categories-header flex justify-between items-center">
                 <div>
-                    <h1 class="font-bold text-3xl mb-3">Latest articles</h1>
+                    <h1 class="font-bold text-3xl mb-4 ">Latest articles</h1>
                     <p>Explore our Free Articles</p>
                 </div>
                 <router-link to="allcategories">
@@ -12,117 +12,70 @@
                     </el-button>
                 </router-link>
             </div>
-            <div class="latest-content py-14 flex gap-8">
-                <router-link to="/learnpress"
-                    class="card flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
+            <div class="latest-content py-14 flex gap-8 lg:flex-row flex-col">
+                <router-link to="/latest"
+                    class="card group flex flex-col rounded-xl bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
                     <div class="card-header">
-                        <img src="/featurecatigories/learnpress.jpg" alt="learnpress"
+                        <img src="/latest/Rectangle_141.svg" alt="learnpress"
                             class="w-full h-64 object-cover rounded-t-lg" />
                     </div>
-                    <div class="card-content flex-1 flex flex-col justify-end p-6">
+                    <div class="card-conten p-6">
                         <h2
-                            class="mb-auto font-bold text-xl transition-colors duration-300 group-hover:text-orange-500 ">
-                            Create An LMS Website With LearnePress
+                            class="text-pretty mb-4 font-bold text-xl transition-colors duration-300 group-hover:text-orange-500 ">
+                            Best LearnPress WordPress Theme Collection for 2023
                         </h2>
-                        <div class="flex items-center text-sm text-gray-600 my-8">
-                            <div class="flex items-center mr-12!">
-                                <el-icon class="mr-2! text-orange-500! text-xl!">
-                                    <i class="fas fa-clock"></i>
-                                </el-icon>
-                                <span>2 Weeks</span>
-                            </div>
-                            <div class="flex items-center">
-                                <el-icon class="mr-2! text-orange-500! text-xl!">
-                                    <i class="fas fa-graduation-cap"></i>
-                                </el-icon>
-                                <span>156 Students</span>
-                            </div>
+                        <div class="flex items-center gap-2 mb-4">
+                            <el-icon style="font-size: 25px; color:orange;">
+                                <Calendar />
+                            </el-icon>
+                            <p>Jan 24, 2023</p>
                         </div>
-                        <div class="card-line bg-gray-200 h-0.5"></div>
-                        <div class="card-footer flex justify-between items-center mt-4">
-                            <div class="flex">
-                                <p class="line-through! text-gray-500 me-2">$29.0</p>
-                                <span class="text-green-500 font-bold">Free</span>
-                            </div>
-                            <el-button type="text" class="text-orange-500! flex items-center">
-                                View More
-                            </el-button>
-                        </div>
+                        <p class="text-pretty text-gray-500">Looking for an amazing & well-functional LearnPress
+                            WordPress Theme?...</p>
                     </div>
                 </router-link>
 
-                <router-link to="/learnpress"
-                    class="card flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
+                <router-link to="/latest"
+                    class="card group flex flex-col rounded-xl bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
                     <div class="card-header">
-                        <img src="/featurecatigories/thimpress.jpg" alt="learnpress"
+                        <img src="/latest/Rectangle_140.svg" alt="learnpress"
                             class="w-full h-64 object-cover rounded-t-lg" />
                     </div>
-                    <div class="card-content flex-1 flex flex-col justify-end p-6">
+                    <div class="card-conten p-6">
                         <h2
-                            class="mb-auto font-bold text-xl transition-colors duration-300 group-hover:text-orange-500">
-                            Design A Website With ThimPress
+                            class="text-pretty mb-4 font-bold text-xl transition-colors duration-300 group-hover:text-orange-500 ">
+                            Best LearnPress WordPress Theme Collection for 2023
                         </h2>
-                        <div class="flex items-center text-sm text-gray-600 my-8">
-                            <div class="flex items-center mr-12!">
-                                <el-icon class="mr-2! text-orange-500! text-xl!">
-                                    <i class="fas fa-clock"></i>
-                                </el-icon>
-                                <span>2 Weeks</span>
-                            </div>
-                            <div class="flex items-center">
-                                <el-icon class="mr-2! text-orange-500! text-xl!">
-                                    <i class="fas fa-graduation-cap"></i>
-                                </el-icon>
-                                <span>156 Students</span>
-                            </div>
+                        <div class="flex items-center gap-2 mb-4">
+                            <el-icon style="font-size: 25px; color:orange;">
+                                <Calendar />
+                            </el-icon>
+                            <p>Jan 24, 2023</p>
                         </div>
-                        <div class="card-line bg-gray-200 h-0.5"></div>
-                        <div class="card-footer flex justify-between items-center mt-4">
-                            <div class="flex">
-                                <p class="!line-through text-gray-500 me-2">$59.0</p>
-                                <span class="text-red-500 font-bold">$49.0</span>
-                            </div>
-                            <el-button type="text" class="!text-orange-500 flex items-center">
-                                View More
-                            </el-button>
-                        </div>
+                        <p class="text-pretty text-gray-500">Looking for an amazing & well-functional LearnPress
+                            WordPress Theme?...</p>
                     </div>
                 </router-link>
 
-                <router-link to="/learnpress"
-                    class="card flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
+                <router-link to="/latest"
+                    class="card group flex flex-col rounded-xl bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
                     <div class="card-header">
-                        <img src="/featurecatigories/image.jpg" alt="learnpress"
-                            class="w-full h-64 object-cover rounded-t-lg" />
+                        <img src="/latest/Rectangle_139.svg" alt="learnpress"
+                            class="w-full h-64 object-cover rounded-t-lg offset-top" />
                     </div>
-                    <div class="card-content flex-1 flex flex-col justify-end p-6">
-                        <h2 class="mb-2 font-bold text-xl transition-colors duration-300 group-hover:text-orange-500">
-                            Create
-                            An LMS Website With LearnePress</h2>
-                        <div class="flex items-center text-sm text-gray-600 my-8">
-                            <div class="flex items-center !mr-12">
-                                <el-icon class="!mr-2 !text-orange-500 !text-xl">
-                                    <i class="fas fa-clock"></i>
-                                </el-icon>
-                                <span>2 Weeks</span>
-                            </div>
-                            <div class="flex items-center">
-                                <el-icon class="!mr-2 !text-orange-500 !text-xl">
-                                    <i class="fas fa-graduation-cap"></i>
-                                </el-icon>
-                                <span>156 Students</span>
-                            </div>
+                    <div class="card-conten p-6">
+                        <h2
+                            class="text-pretty mb-4 font-bold text-xl transition-colors duration-300 group-hover:text-orange-500 ">
+                            Best LearnPress WordPress Theme Collection for 2023
+                        </h2>
+                        <div class="flex items-center gap-2 mb-4">
+                            <el-icon style="font-size: 25px; color:orange;">
+                                <Calendar />
+                            </el-icon>
+                            <p>Jan 24, 2023</p>
                         </div>
-                        <div class="card-line bg-gray-200 h-0.5"></div>
-                        <div class="card-footer flex justify-between items-center mt-4">
-                            <div class="flex">
-                                <p class="!line-through text-gray-500 me-2">$29.0</p>
-                                <span class="text-green-500 font-bold">Free</span>
-                            </div>
-                            <el-button type="text" class="!text-orange-500 flex items-center">
-                                View More
-                            </el-button>
-                        </div>
+                        <p class="text-pretty text-gray-500">Looking for an amazing & well-functional LearnPress
+                            WordPress Theme?...</p>
                     </div>
                 </router-link>
             </div>
