82583e129cc377de8694e2968165ca83d82d609e
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-09 23:08:12 +0500

    course was created

---
+++ src/views/courselisting/Course.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <div class="container px-5 mx-auto py-10">
+        <!-- Main Content Grid -->
+        <div class="grid grid-cols-1 lg:grid-cols-4 gap-10">
+
+            <!-- Course List -->
+            <div class="lg:col-span-3">
+                <CourseList />
+            </div>
+
+            <!-- Sidebar -->
+            <div class="lg:col-span-1">
+                <CourseSidebar />
+            </div>
+
+            <div class="col-span-1">
+                <CoursePagination />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import CourseSidebar from './CourseSidebar.vue';
+import CourseList from './CourseList.vue';
+import CoursePagination from './CoursePagination.vue';
+</script>
