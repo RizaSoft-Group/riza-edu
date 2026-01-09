90397c6c554de5cdab422d9f5770807a97265642
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-09 23:09:33 +0500

    hover update

--- src/views/homepage/FeaturedCourses.vue
+++ src/views/homepage/FeaturedCourses.vue
@@ -5,7 +5,7 @@
                 <h1 class="font-bold text-3xl mb-3">Featured Courses</h1>
                 <p>Explore our Popular Categories</p>
             </div>
-            <router-link to="allcourses">
+            <router-link to="course">
                 <el-button type="warning" round size="large">
                     All Courses
                 </el-button>
@@ -14,7 +14,7 @@
         <div class="courses-cards grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8 mt-10">
             <router-link 
                 to="/learnpress"
-                class="card flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
+                class="card group flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
                 <div class="card-header">
                     <img 
                         src="/featurecatigories/learnpress.jpg" 
@@ -54,7 +54,7 @@
             </router-link>
 
             <router-link to="/learnpress"
-                class="card flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
+                class="card group flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
                 <div class="card-header">
                     <img src="/featurecatigories/thimpress.jpg" alt="learnpress"
                         class="w-full h-64 object-cover rounded-t-lg" />
@@ -80,10 +80,10 @@
                     <div class="card-line bg-gray-200 h-0.5"></div>
                     <div class="card-footer flex justify-between items-center mt-4">
                         <div class="flex">
-                            <p class="!line-through text-gray-500 me-2">$59.0</p>
+                            <p class="line-through! text-gray-500 me-2">$59.0</p>
                             <span class="text-red-500 font-bold">$49.0</span>
                         </div>
-                        <el-button type="text" class="!text-orange-500 flex items-center">
+                        <el-button type="text" class="text-orange-500! flex items-center">
                             View More
                         </el-button>
                     </div>
@@ -91,7 +91,7 @@
             </router-link>
 
             <router-link to="/learnpress"
-                class="card flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
+                class="card group flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
                 <div class="card-header">
                     <img src="/featurecatigories/image.jpg" alt="learnpress"
                         class="w-full h-64 object-cover rounded-t-lg" />
@@ -100,14 +100,14 @@
                     <h2 class="mb-2 font-bold text-xl transition-colors duration-300 group-hover:text-orange-500">Create
                         An LMS Website With LearnePress</h2>
                     <div class="flex items-center text-sm text-gray-600 my-8">
-                        <div class="flex items-center !mr-12">
-                            <el-icon class="!mr-2 !text-orange-500 !text-xl">
+                        <div class="flex items-center mr-12!">
+                            <el-icon class="mr-2! text-orange-500! text-xl!">
                                 <i class="fas fa-clock"></i>
                             </el-icon>
                             <span>2 Weeks</span>
                         </div>
                         <div class="flex items-center">
-                            <el-icon class="!mr-2 !text-orange-500 !text-xl">
+                            <el-icon class="mr-2! text-orange-500! text-xl!">
                                 <i class="fas fa-graduation-cap"></i>
                             </el-icon>
                             <span>156 Students</span>
@@ -116,10 +116,10 @@
                     <div class="card-line bg-gray-200 h-0.5"></div>
                     <div class="card-footer flex justify-between items-center mt-4">
                         <div class="flex">
-                            <p class="!line-through text-gray-500 me-2">$29.0</p>
+                            <p class="line-through! text-gray-500 me-2">$29.0</p>
                             <span class="text-green-500 font-bold">Free</span>
                         </div>
-                        <el-button type="text" class="!text-orange-500 flex items-center">
+                        <el-button type="text" class="text-orange-500! flex items-center">
                             View More
                         </el-button>
                     </div>
@@ -127,7 +127,7 @@
             </router-link>
 
             <router-link to="/learnpress"
-                class="card flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
+                class="card group flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
                 <div class="card-header">
                     <img src="/featurecatigories/image-1.jpg" alt="learnpress"
                         class="w-full h-64 object-cover rounded-t-lg" />
@@ -136,14 +136,14 @@
                     <h2 class="mb-2 font-bold text-xl transition-colors duration-300 group-hover:text-orange-500">Create
                         An LMS Website With LearnePress</h2>
                     <div class="flex items-center text-sm text-gray-600 my-8">
-                        <div class="flex items-center !mr-12">
-                            <el-icon class="!mr-2 !text-orange-500 !text-xl">
+                        <div class="flex items-center mr-12!">
+                            <el-icon class="mr-2! text-orange-500! text-xl!">
                                 <i class="fas fa-clock"></i>
                             </el-icon>
                             <span>2 Weeks</span>
                         </div>
                         <div class="flex items-center">
-                            <el-icon class="!mr-2 !text-orange-500 !text-xl">
+                            <el-icon class="mr-2! text-orange-500! text-xl!">
                                 <i class="fas fa-graduation-cap"></i>
                             </el-icon>
                             <span>156 Students</span>
@@ -152,10 +152,10 @@
                     <div class="card-line bg-gray-200 h-0.5"></div>
                     <div class="card-footer flex justify-between items-center mt-4">
                         <div class="flex">
-                            <p class="!line-through text-gray-500 me-2">$29.0</p>
+                            <p class="line-through! text-gray-500 me-2">$29.0</p>
                             <span class="text-green-500 font-bold">Free</span>
                         </div>
-                        <el-button type="text" class="!text-orange-500 flex items-center">
+                        <el-button type="text" class="text-orange-500! flex items-center">
                             View More
                         </el-button>
                     </div>
@@ -163,7 +163,7 @@
             </router-link>
 
             <router-link to="/learnpress"
-                class="card flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
+                class="card group flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
                 <div class="card-header">
                     <img src="/featurecatigories/image-2.png" alt="learnpress"
                         class="w-full h-64 object-cover rounded-t-lg" />
@@ -172,14 +172,14 @@
                     <h2 class="mb-2 font-bold text-xl transition-colors duration-300 group-hover:text-orange-500">Create
                         An LMS Website With LearnePress</h2>
                     <div class="flex items-center text-sm text-gray-600 my-8">
-                        <div class="flex items-center !mr-12">
-                            <el-icon class="!mr-2 !text-orange-500 !text-xl">
+                        <div class="flex items-center mr-12!">
+                            <el-icon class="mr-2! text-orange-500! text-xl!">
                                 <i class="fas fa-clock"></i>
                             </el-icon>
                             <span>2 Weeks</span>
                         </div>
                         <div class="flex items-center">
-                            <el-icon class="!mr-2 !text-orange-500 !text-xl">
+                            <el-icon class="mr-2! text-orange-500! text-xl!">
                                 <i class="fas fa-graduation-cap"></i>
                             </el-icon>
                             <span>156 Students</span>
@@ -188,10 +188,10 @@
                     <div class="card-line bg-gray-200 h-0.5"></div>
                     <div class="card-footer flex justify-between items-center mt-4">
                         <div class="flex">
-                            <p class="!line-through text-gray-500 me-2">$29.0</p>
+                            <p class="line-through! text-gray-500 me-2">$29.0</p>
                             <span class="text-green-500 font-bold">Free</span>
                         </div>
-                        <el-button type="text" class="!text-orange-500 flex items-center">
+                        <el-button type="text" class="text-orange-500! flex items-center">
                             View More
                         </el-button>
                     </div>
@@ -199,7 +199,7 @@
             </router-link>
 
             <router-link to="/learnpress"
-                class="card flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
+                class="card group flex flex-col rounded-lg bg-white shadow-sm hover:shadow-lg hover:-translate-y-5 transition duration-300 transform">
                 <div class="card-header">
                     <img src="/featurecatigories/image-3.jpg" alt="learnpress"
                         class="w-full h-64 object-cover rounded-t-lg" />
@@ -208,14 +208,14 @@
                     <h2 class="mb-2 font-bold text-xl transition-colors duration-300 group-hover:text-orange-500">Create
                         An LMS Website With LearnePress</h2>
                     <div class="flex items-center text-sm text-gray-600 my-8">
-                        <div class="flex items-center !mr-12">
-                            <el-icon class="!mr-2 !text-orange-500 !text-xl">
+                        <div class="flex items-center mr-12!">
+                            <el-icon class="mr-2! text-orange-500! text-xl!">
                                 <i class="fas fa-clock"></i>
                             </el-icon>
                             <span>2 Weeks</span>
                         </div>
                         <div class="flex items-center">
-                            <el-icon class="!mr-2 !text-orange-500 !text-xl">
+                            <el-icon class="mr-2! text-orange-500! text-xl!">
                                 <i class="fas fa-graduation-cap"></i>
                             </el-icon>
                             <span>156 Students</span>
@@ -224,10 +224,10 @@
                     <div class="card-line bg-gray-200 h-0.5"></div>
                     <div class="card-footer flex justify-between items-center mt-4">
                         <div class="flex">
-                            <p class="!line-through text-gray-500 me-2">$29.0</p>
+                            <p class="line-through! text-gray-500 me-2">$29.0</p>
                             <span class="text-green-500 font-bold">Free</span>
                         </div>
-                        <el-button type="text" class="!text-orange-500 flex items-center">
+                        <el-button type="text" class="text-orange-500! flex items-center">
                             View More
                         </el-button>
                     </div>
