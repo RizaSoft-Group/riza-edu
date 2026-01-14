65c547c1f898c89195efa869abb7256cad809a97
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-14 20:13:24 +0500

    Pagination created

--- src/views/courselisting/CoursePagination.vue
+++ src/views/courselisting/CoursePagination.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container mx-auto">
-        <div class="pagination w-full flex items-center border-2">
-            <el-pagination size="small" background layout="prev, pager, next" :total="50" class="mx-auto py-2" />
+        <div class="pagination w-full flex items-center m-3">
+            <el-pagination round background layout="prev, pager, next" :total="50" class="mx-auto py-2" />
         </div>
     </div>
 </template>
