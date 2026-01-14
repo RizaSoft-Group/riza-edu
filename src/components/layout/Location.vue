415654108e3170b42935aee5ce197514dcc3bbb0
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-14 20:15:18 +0500

    location created

---
+++ src/components/layout/Location.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="name !== 'Home'" class="location w-full h-15 bg-gray-100">
+        <div class="container h-full px-5 mx-auto flex items-center">
+            <nav class="flex items-center gap-2 text-sm text-gray-600">
+                <router-link to="/" class="hover:text-orange-500 font-medium">
+                    Home
+                </router-link>
+                <span>></span>
+                <span @click="$route.push('/ {{ name }}')" class="text-gray-00 font-medium">
+                    {{ name }}
+                </span>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        name() {
+            return this.$route.name || 'Page'
+        }
+    }
+}
+</script>
