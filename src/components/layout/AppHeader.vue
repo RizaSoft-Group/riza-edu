ce54b90a26f98813d9c20657d361926bef5025a4
bw007 <mr.khasanov02@gmail.com>
2026-01-14 09:10:45 +0500

    style: update AppHeader layout and improve responsiveness

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="container px-5 mx-auto flex justify-between gap-3 items-center h-15">
-    <el-icon class="lg:hidden! cursor-pointer hover:opacity-75 transition-opacity duration-150">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
-        <path d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z"/>
-      </svg>
-    </el-icon>
-    <router-link to="/" class="mr-auto">
-      <img src="/logo.svg" alt="Brand Logo" />
+  <div class="container px-5 mx-auto flex justify-between gap-4 items-center h-15">
+    <app-sidebar></app-sidebar>
+    <router-link to="/" class="mr-auto lg:mr-0">
+      <img
+        src="/logo.svg" 
+        class="w-32 lg:w-41.75" 
+        alt="Brand Logo"
+      />
     </router-link>
     <el-menu
       :default-active="activeIndex"
@@ -32,11 +32,13 @@
       <el-menu-item index="5">Premium Theme</el-menu-item>
     </el-menu>
     <div class="flex items-center gap-3 sm:gap-5">
-      <el-icon @click="toggleTheme()" class="cursor-pointer hover:opacity-75 transition-opacity duration-150">
+      <el-icon @click="toggleTheme()" class="hidden! sm:block! cursor-pointer hover:opacity-75 transition-opacity duration-150">
         <Sunny color="#303133" v-if="currentTheme !== 'dark'" />
         <Moon color="#303133" v-else />
       </el-icon>
-      <lang-select></lang-select>
+      <div class="hidden! sm:block!">
+        <lang-select></lang-select>
+      </div>
       <el-icon
         class="cursor-pointer hover:text-[#E6A23C]! transition-colors duration-300"
         color="#303133"
@@ -52,6 +54,8 @@
 
 <script setup>
 import { ref } from 'vue'
+
+import AppSidebar from './AppSidebar.vue';
 import LangSelect from '../ui/LangSelect.vue';
 
 const activeIndex = ref('0')
@@ -65,21 +69,3 @@ const toggleTheme = () => {
   console.log('Toggle theme to:', currentTheme.value)
 }
 </script>
-
-<style>
-.el-menu--horizontal .el-menu-item:not(.is-disabled):focus,
-.el-menu--horizontal .el-menu-item:not(.is-disabled):hover {
-  color: #E6A23C !important;
-  background-color: #F5F5F5 !important;
-}
-
-.el-menu--horizontal .el-menu-item.is-active,
-.el-menu--horizontal .el-sub-menu.is-active {
-  --el-menu-active-color: #E6A23C !important;
-  border-color: var(--el-menu-active-color) !important;
-}
-
-.el-menu--horizontal .el-sub-menu:hover .el-sub-menu__title {
-  --el-menu-hover-text-color: #E6A23C !important;
-}
-</style>
