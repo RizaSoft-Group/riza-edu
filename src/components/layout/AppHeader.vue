55a0c6253ca7a42d80c5fdf7dd833603f2fead64
bw007 <mr.khasanov02@gmail.com>
2026-01-21 01:06:16 +0500

    Update AppHeader.vue: Refactor menu items for better structure and add indices

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -8,26 +8,31 @@
         alt="Brand Logo"
       />
     </router-link>
-    <el-menu :default-active="activeIndex" mode="horizontal" class="border-none! app-menu lg:flex! hidden!"
-      :ellipsis="false" @select="handleSelect">
-      <el-menu-item  @click="$router.push('/')">
+    <el-menu
+      :default-active="activeIndex"
+      mode="horizontal"
+      class="border-none! app-menu lg:flex! hidden!"
+      :ellipsis="false"
+      @select="handleSelect"
+    >
+      <el-menu-item index="0" @click="$router.push('/')">
         Home
       </el-menu-item>
-      <el-menu-item  @click="$router.push('/course')">Course</el-menu-item>
-      <el-menu-item  @click="$router.push('/blog')">Blog</el-menu-item>
-      <el-sub-menu  @click="$router.push('/page')">
+      <el-menu-item index="1" @click="$router.push('/course')">Course</el-menu-item>
+      <el-menu-item index="2" @click="$router.push('/blog')">Blog</el-menu-item>
+      <el-sub-menu index="3">
         <template #title>Page</template>
-        <el-menu-item @click="$router.push('/contact')">Contact</el-menu-item>
-        <el-menu-item @click="$router.push('/faqs')">FAQs</el-menu-item>
-        <el-menu-item @click="$router.push('/error')">Error</el-menu-item>
-        <el-sub-menu index="2-4">
+        <el-menu-item index="3-1" @click="$router.push('/contact')">Contact</el-menu-item>
+        <el-menu-item index="3-2" @click="$router.push('/faqs')">FAQs</el-menu-item>
+        <el-menu-item index="3-3" @click="$router.push('/error')">Error</el-menu-item>
+        <el-sub-menu index="3-4">
           <template #title>item four</template>
-          <el-menu-item index="2-4-1">item one</el-menu-item>
-          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="3-4-1">item one</el-menu-item>
+          <el-menu-item index="3-4-2">item two</el-menu-item>
         </el-sub-menu>
       </el-sub-menu>
-      <el-menu-item @click="$router.push('/learnepress')">LearnPress Add-On</el-menu-item>
-      <el-menu-item @click="$router.push('/premium')">Premium Theme</el-menu-item>
+      <el-menu-item index="4" @click="$router.push('/learnepress')">LearnPress Add-On</el-menu-item>
+      <el-menu-item index="5" @click="$router.push('/premium')">Premium Theme</el-menu-item>
     </el-menu>
     <div class="flex items-center gap-3 sm:gap-5">
       <el-icon @click="toggleTheme()" class="hidden! sm:block! cursor-pointer hover:opacity-75 transition-opacity duration-150">
@@ -38,7 +43,7 @@
       <el-icon class="cursor-pointer hover:text-[#E6A23C]! transition-colors duration-300" color="#303133">
         <Search /> 
       </el-icon>
-      <el-button round type="warning" @click="$router.push('/login')">Login</el-button>
+      <el-button round type="warning" @click="$router.push('auth/login')">Login</el-button>
     </div>
   </div>
 </template>
