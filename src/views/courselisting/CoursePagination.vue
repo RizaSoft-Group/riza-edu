931e7a3ce2a0a1bf1e871a927cdcba8bcf75900c
Shaxzod <isomiddinovshaxzod007@gmail.com>
2026-01-23 09:27:15 +0500

    all moved into components

--- src/views/courselisting/CoursePagination.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="container mx-auto">
-        <div class="pagination w-full flex items-center m-3">
-            <el-pagination round background layout="prev, pager, next" :total="50" class="mx-auto py-2" />
-        </div>
-    </div>
-</template>
