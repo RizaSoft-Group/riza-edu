1e2803789d2800aef76e0cda9ce9c2973f43f513
bw007 <mr.khasanov02@gmail.com>
2026-01-28 20:15:39 +0500

    feat: add SignInView component for user authentication

--- src/views/auth/SignInView.vue
+++ src/views/auth/SignInView.vue
@@ -8,7 +8,7 @@
         </div>
       </div>
       <!-- right content -->
-      <div class="content-section flex flex-col justify-center items-center p-8 bg-white md:col-span-2 relative">
+      <div class="flex flex-col justify-center items-center p-8 bg-white md:col-span-2 relative">
         <!-- Back to Home Button -->
         <el-button type="primary" @click="$router.push('/')" class="absolute! top-5 right-5">
           <el-icon :size="18">
@@ -20,18 +20,16 @@
         <div class="w-full max-w-md mx-auto">
           <!-- Toggle Buttons - FIXED POSITION -->
           <div class="flex items-center justify-between gap-4 mb-8">
-            <h3 class="text-gray-600 text-sm sm:text-base whitespace-nowrap">
+            <!-- <h3 class="text-gray-600 text-sm sm:text-base whitespace-nowrap">
               {{
                 isLogin ? "Don't have an account?" : "Already have an account?"
               }}
-            </h3>
-            <el-button type="primary" size="large" @click="toggleForm">
-              {{ isLogin ? "SIGN UP" : "LOGIN" }}
-            </el-button>
+            </h3> -->
+            <el-button type="primary" size="large">Sign In</el-button>
           </div>
           <!-- Component Render -->
           <Transition name="fade" mode="out-in">
-            <component :is="isLogin ? Login : Register"/>
+            <component :is="Login"/>
           </Transition>
         </div>
       </div>
@@ -40,26 +38,13 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
 import { ArrowLeft } from "@element-plus/icons-vue";
 import Login from "@/components/auth/LoginView.vue";
-import Register from "@/components/auth/RegisterView.vue";
+import { useAuthStore } from "@/stores/auth/useAuthStore";
 
-const isLogin = ref(true);
-
-
-const toggleForm = () => {
-  isLogin.value = !isLogin.value;
-};
 </script>
 
 <style scoped>
-.login {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, sans-serif;
-}
-
-/* Responsive */
 @media (max-width: 1024px) {
   .image-section {
     display: none;
